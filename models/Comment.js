cfbc131174579060fa1d58063800d57ee242c025
Jennie Allen <jenanemone@gmail.com>
2022-09-28 21:01:46 -0500

    built out more file structure

--- models/Comment.js
+++ models/Comment.js
@@ -21,29 +21,3 @@ const CommentSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
-/* 
-const CommentSchema = new mongoose.Schema({
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Post"
-  },
-  comment: {
-    type: String,
-    required: true
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  likes: {
-    type: Number,
-    default: 0
-  },
-});
-
-module.exports = mongoose.model("Comment", CommentSchema);
-*/
