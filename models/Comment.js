f4eba1aa1f68266e3fa5f5951e89167c33db3613
Jennie Allen <jenanemone@gmail.com>
2022-09-28 20:46:10 -0500

    started changing to mongoose

---
+++ models/Comment.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    commentText: {
+        type: String,
+        required: true,
+    },
+    upvote: {
+        type: Number,
+        default: 1,
+    },
+    downvote: {
+        type: Number,
+        default: 0
+    },   
+
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
+/* 
+const CommentSchema = new mongoose.Schema({
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
+  },
+  comment: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
+*/
